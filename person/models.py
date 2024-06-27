c18f1c7bee7318211be0bbe67956b479790b234f
Mirabbos995 <jahonaliyevmirabbos621@gmail.com>
2024-06-27 16:21:19 +0500

    ed

--- person/models.py
+++ person/models.py
@@ -19,4 +19,4 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-#https://www.canva.com/design/DAGEMgniEdE/Nxd7O6eawlK1PbD5jPSAaA/edit
+"https://www.canva.com/design/DAGEMgniEdE/Nxd7O6eawlK1PbD5jPSAaA/edit"
